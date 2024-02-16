661137c9b6643401a01c33a2f0c7041dc9ce215c
The-real-joe <j.boucher690@gmail.com>
2024-02-16 16:57:18 -0500

    done

---
+++ src/reducers/gifReducers.js
@@ -0,0 +1,34 @@
+import { SEARCH_SUCCESS, SEARCH_ERROR, SET_IS_FETCHING } from "../actions/GifAction";
+import data from "../data/gifs";
+
+const initialState = {
+    data,
+    isFetching: false,
+    error: ""
+};
+
+export const reducer = (state= initialState, action) => {
+  switch(action.type) {
+    case SET_IS_FETCHING:
+        return {
+            ...state,
+            isFetching: action.[ayload]
+        }
+        case SEARCH_SUCCESS:
+            return {
+                ...state,
+
+                isFetching: false,
+                data: action.payload,
+                error: ""
+            }
+        case SEARCH_ERROR:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+            default:
+                return state
+  }
+}
